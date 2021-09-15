19620dd495bad91a9648d88fd32212a021918d25
Zak <ataxored@gmail.com>
2021-09-15 09:04:41 -0700

    Added logo and animations

--- app/src/main/java/com/flat/internal/SplashActivity.kt
+++ app/src/main/java/com/flat/internal/SplashActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.google.android.material.imageview.ShapeableImageView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        var Logo : ShapeableImageView = findViewById(R.id.Logo);
+        Logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.logo))
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             this.finish()
-        }, 3000)
+        }, 1300)
     }
 }
--- app/src/main/java/com/flat/internal/VerificationFragment.kt
+++ app/src/main/java/com/flat/internal/VerificationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -16,10 +17,11 @@ class VerificationFragment : BaseFragment(R.layout.fragment_verification) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val VerifyTextView : TextView = view.findViewById(R.id.VerifyTextView)
+        VerifyTextView.setText(getString(R.string.code_has_sended) + " " + AuthFragment.AuthSingleTon!!.PhoneNum)
+
         view.findViewById<Button>(R.id.VerifyPhoneNumber).setOnClickListener {
-
             val VerificationCodeEditText : EditText = view.findViewById(R.id.VerificationCodeEditText)
-
             AuthFragment.AuthSingleTon!!.VerifyPhoneNumber(VerificationCodeEditText.text.toString())
         }
     }
